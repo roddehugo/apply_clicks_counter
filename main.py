e208c984f5a320f368a6165cc1163d9a3415c5c8
Hugo Rodde <rodde.hugo@gmail.com>
2015-02-06 19:02:18 +0100

    Threading

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+import logging
 import Queue
 import os
 import requests
@@ -14,7 +15,6 @@ PYTHON_ROOT = os.path.dirname(os.path.realpath(__file__))
 COMMAND = "%s/display_text.app" % PYTHON_ROOT
 
 values_queue = Queue.Queue()
-processes = []
 lock_socket = None
 
 
@@ -24,10 +24,11 @@ def exit_gracefully(signum, frame):
     signal.signal(signal.SIGINT, original_sigint)
 
     def exit():
-        print 'Waiting for threads to terminate...'
+        logging.info('Waiting for threads to terminate...')
         request.stop()
         request.join()
-        lock_socket.close()
+        if lock_socket:
+            lock_socket.close()
         sys.exit(1)
 
     try:
@@ -44,9 +45,9 @@ def get_lock(process_name):
     lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
     try:
         lock_socket.bind('\0' + process_name)
-        print '[Main] %s got the lock' % process_name
+        logging.info('%s got the lock' % process_name)
     except socket.error:
-        print '[Main] %s lock exists' % process_name
+        logging.info('%s lock exists' % process_name)
         sys.exit()
 
 
@@ -60,11 +61,13 @@ def center(val, max_chars=13):
 
 def get_apply_clicks():
     try:
-        res = requests.get('https://w4u-test-app.work4labs.com/w4d/api/v1/global/stats?format=json')
+        res = requests.get('https://www.random.org/integers/?num=1&min=1&max=60000&col=1&base=10&format=plain&rnd=new')
+        # res = requests.get('https://w4u-test-app.work4labs.com/w4d/api/v1/global/stats?format=json')
     except Exception:
         return None
     if res:
-        return str(json.loads(res.text).get('count', None))
+        # return str(json.loads(res.text).get('count', None))
+        return str(res.text)
     return None
 
 
@@ -81,60 +84,85 @@ class StoppableThread(threading.Thread):
         return self._stop.isSet()
 
 
-class DisplayThread(StoppableThread):
-
-    def __init__(self, queue, seconds=60):
-        super(DisplayThread, self).__init__()
-        self.prg = None
-        self.s = seconds
-        values = queue.get(block=True)
-        self.command = [COMMAND, "-s", seconds, "-f", values[0], "-t", values[1]]
-
-    def run(self):
-        print "[DT] Display On"
-        print self.command
-        time.sleep(self.s)
-        # self.prg = subprocess.Popen(current_command, shell=False)
-        print "[DT] Display Off"
-
-
 class RequestThread(StoppableThread):
+    name = "RequestThread"
+
     def __init__(self, queue):
         super(RequestThread, self).__init__()
         self.queue = queue
 
     def run(self):
         old_value = ""
+        now = time.time()
         while not self.stopped():
-            print "[RT] GET AC"
-            current_value = get_apply_clicks() or old_value
-            if current_value != old_value:
-                print "[RT] New value"
-                self.queue.put((old_value, current_value))
-                old_value = current_value
-            time.sleep(60)
+            while time.time() > now + 10 or old_value == "":
+                logging.info("GET AC value")
+                value = get_apply_clicks() or old_value
+                now = time.time()
+                if value != old_value:
+                    logging.info("New value %s", value)
+                    self.queue.put(value)
+                    old_value = value
+            time.sleep(0.01)
+
+    def fetch_and_put_value_in_queue(self):
+        logging.info("GET AC value")
+        value = get_apply_clicks() or None
+        if value:
+            logging.info("New value %s", value)
+            self.queue.put(value)
+        return value
+
+
+class DisplayThread(StoppableThread):
+    name = "DisplayThread"
+
+    def __init__(self, queue, seconds=60):
+        super(DisplayThread, self).__init__()
+        self.prg = None
+        self.sec = seconds
+        try:
+            value = queue.get(block=True, timeout=120)
+        except Queue.Empty:
+            self.stop()
+        self.command = [COMMAND, "-s", seconds, "-t", value]
+
+    def run(self):
+        if not self.stopped():
+            logging.info("Display On")
+            logging.info(self.command)
+            time.sleep(self.sec)
+            self.prg = subprocess.Popen(self.command, shell=False)
+            logging.info("Display Off")
 
 
 if __name__ == '__main__':
-    get_lock('apply_clicks_counter_1')
+    logging.basicConfig(
+        level=logging.INFO,
+        format='(%(threadName)-10s) %(message)s',
+    )
+    # get_lock('apply_clicks_counter_3')
     # store the original SIGINT handler
     original_sigint = signal.getsignal(signal.SIGINT)
     signal.signal(signal.SIGINT, exit_gracefully)
-    values_queue.put(("Work4Labs", "AC Counter"))
-    display = DisplayThread(values_queue, 4)
+
+    values_queue.put("Work4Labs")
+    values_queue.put("AC Counter")
 
     request = RequestThread(values_queue)
-
-    display.start()
     request.start()
 
+    display = DisplayThread(values_queue, 2)
+    display.start()
+    display.join()
+
+    display = DisplayThread(values_queue, 2)
+    display.start()
     display.join()
 
     while request.is_alive():
-        pass
-        # display = DisplayThread(values_queue)
-        # display.setDaemon(True)
-        # display.start()
-        # display.join()
+        display = DisplayThread(values_queue)
+        display.start()
+        display.join()
 
     request.join()
